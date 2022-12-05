b1090eed2e67ba9eb298d96c81d2546ab61351ae
dawe <danielweber1992@outlook.de>
2022-12-05 23:18:46 +0100

    workout definition initial

--- src/shared/models/Workout.ts
+++ src/shared/models/Workout.ts
@@ -1,3 +1,33 @@
-export interface Workout {
+import {Exercise} from './Exercise';
 
+export interface Workout {
+    units: WorkoutUnit[];
+}
+
+interface WorkoutUnit {
+    day: Day;
+    type: WorkoutType;
+    exerciseList: Exercise[];
+}
+
+enum Day {
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7,
+}
+
+enum WorkoutType {
+    CHEST = 'Chest',
+    BACK = 'Back',
+    LEG = 'Leg',
+    SHOULDER = 'Shoulder',
+    NECK = 'Neck',
+    BICEPS = 'Biceps',
+    Triceps = 'Triceps',
+    ABS = 'Abs',
+    REST = 'Rest'
 }
