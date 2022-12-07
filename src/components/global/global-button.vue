c83195f8ad05c8f102b16796f7c0662b9b9ce89f
DAN92DE <danielweber1992@outlook.de>
2022-12-07 23:39:50 +0100

    changes global button

--- src/components/global/global-button.vue
+++ src/components/global/global-button.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
+import {computed} from 'vue';
 
 const props = defineProps<{
   text: string,
-  btnCls?: 'btn-primary' | 'btn-secondary',
+  btnType?: 'btn-primary' | 'btn-secondary',
+  btnCls?: string,
   type?: 'button' | 'submit' | 'reset'
 }>();
 
+const buttonClasses = computed<string[]>(() => {
+  return [props.btnType ? props.btnType : 'btn-primary', props.btnCls]
+});
 </script>
 
 <template>
-  <button :type="props.type ? props.type : 'button'"
-          :class="props.btnCls ? props.btnCls : 'btn-primary'">
+  <button :type="props.type ? props.type : 'button'" :class="buttonClasses">
     {{ props.text}}
   </button>
 </template>
